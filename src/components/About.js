e24abf379b637d487bd48f81132103e458b3bc45
dariomavec <dario.mavec@gmail.com>
2018-03-23 11:46:25 +1100

    Updated with details of my domain, stats and coding experience

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
-class About extends Component {
+export default class About extends Component {
   render() {
     return (
       <div className="About section">
-	  <h3>I am an <strong>experienced data scientist</strong> with a focus on <strong>customer</strong>, <strong>fraud</strong> and <strong>predictive asset management</strong></h3>
+	  <h1><br/>About Me!</h1>
 		<Container>
 			<Row>
 				<Col>
-				<h1>I am an <strong>experienced data scientist</strong> with a focus on <strong>customer</strong>, <strong>fraud</strong> and <strong>predictive asset management</strong></h1>
+				<h3>My name is <strong>Dario Mavec</strong> and data science is my game. I have spent the last 5 years consulting with major <strong>governments, companies</strong> and <strong>organisations</strong> to discover <strong>interesting</strong> and <strong>useful</strong> insights from data.</h3>
 				</Col>
 			</Row>
 			<Row>
@@ -17,19 +17,19 @@ class About extends Component {
 			</Row>
 			<Row>
 				<Col>
-				<h1><i className="fas fa-comments" aria-hidden="true"/></h1>
+				<h1> <i className="fas fa-signal" aria-hidden="true"/> </h1>
 				<br/>
-				I can communicate and stuff.
+				I have studied and implemented a wide range of statistical techniques including logistic, random forest, model matrix approaches, unsupervised segmentation models and more!
 				</Col>
 				<Col>
-				<h1><i className="fas fa-industry" aria-hidden="true"/></h1>
+				<h1> <i className="fas fa-industry" aria-hidden="true"/> </h1>
 				<br/>
-				I have experience with top tier consulting and stuff.
+				I have deep experience working for top-tier consulting firm, Accenture, in challenging projects across the telecommunications, government, resources and entertainment industries.
 				</Col>
 				<Col>
-				<h1><i className="fas fa-laptop" aria-hidden="true"/></h1>
+				<h1> <i className="fas fa-laptop" aria-hidden="true"/> </h1>
 				<br/>
-				I work with computers and stuff.
+				I have delivered projects using a wide range of toolsets (R, Python, SQL, SAS, Alteryx, AWS), often needing to learn and master fresh kits on the fly.
 				</Col>
 			</Row>
 		</Container>
@@ -37,5 +37,3 @@ class About extends Component {
     );
   }
 }
-
-export default About;
