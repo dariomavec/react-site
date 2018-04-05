ca367f2342298dda4f91ad10fe710ba85067505c
dariomavec <dario.mavec@gmail.com>
2018-04-05 11:18:58 +1000

    Reformat and gave credit for starting code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,23 +5,30 @@ import { Container, Row, Col } from 'reactstrap';
 export default class Footer extends Component {
   render() {
     return (
-      <div className="Footer">
 		<Container>	
-			<Row className="align-items-center">
+			<Row>
+				<Col>
+				<h1>Get in touch!</h1>
+				</Col>
+			</Row>
+			<Row className="align-items-center Footer">
 				<Col className="col-12 col-md-6">
 					<img src={footerSplash} className="footer-pic" alt="Footer"></img>
 				</Col>
-				<Col className="col-12 col-md-6">
-					<h1>Get in touch!</h1>
+				<Col className="col-12 col-md-6">					
 					<h3>
 					  <a href="https://linkedin.com/in/dario-mavec" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin i-hov"/></a>  
 					  <a href="https://github.com/dariomavec" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square i-hov"/></a>  
 					  <a href="mailto:dario.mavec@gmail.com?Subject=Greetings!" target="_top"><i className="fas fa-envelope-square i-hov"/></a>
 					</h3>
+					<br/>
+					<p>Built with React by Dario Mavec.<br/><br/>
+					Check out the <a href="https://github.com/dariomavec/site" target="_blank" rel="noopener noreferrer">repo</a>!<br/>
+					Credit to Matthew Thorry's <a href="https://github.com/mthorry/site" target="_blank" rel="noopener noreferrer">work</a> which got me started.
+					</p>
 				</Col>
 			</Row>
 		</Container>
-      </div>
     );
   }
 }
